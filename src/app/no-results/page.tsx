d3170a0894d650e3c11bdc1442e2f55d4918b64c
Emir Risyad <emirrsyd@gmail.com>
2025-08-29 23:11:27 +0200

    fix(no-results): fix button import path

--- src/app/no-results/page.tsx
+++ src/app/no-results/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button } from '@/src/components/ui/button';
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
